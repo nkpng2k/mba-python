ac7dea6884417ab4db0fe9e5f2229c1437f72c19
Nick Png <nkpng2k@gmail.com>
2023-05-13 16:07:00 -0700

    fixed imports, functioning histogram function

---
+++ tepper_stats/tepper_stats/confidence_intervals/__init__.py
@@ -0,0 +1,5 @@
+from .confidence_intervals import calculate_confidence_interval
+
+__all__ = [
+    "calculate_confidence_interval"
+]
--- tepper_stats/tepper_stats/plot/histogram.py
+++ tepper_stats/tepper_stats/plot/histogram.py
@@ -4,29 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from tepper_stats.tepper_stats.confidence_intervals import confidence_intervals as ci
+import tepper_stats.tepper_stats.confidence_intervals as ci
 
 
 def histogram(
         data: Union[np.ndarray, pd.DataFrame, pd.Series] = None,
         alpha: float = None,
-        title: str = "",
-        x_label: str = "",
-        y_label: str = "",
+        bins: Union[str, int] = "auto",
+        title: str = "Histogram",
+        x_label: str = "Frequency",
+        y_label: str = "Value",
 ):
     """
+    Displays a histogram and calculates the confidence interval
+    for the data provided.
 
-    :param data:
-    :param alpha:
-    :param title:
-    :param x_label:
-    :param y_label:
+    :param data: a 1D array or array-like object
+    :param alpha: significance level to use for the confidence interval
+    :param title: title for the histogram
+    :param x_label: label for the x-axis
+    :param y_label: label for the y-axis
     :return:
     """
     if data.ndim != 1:
         raise ValueError("Input data array must be 1D.")
 
-    n, bins, patches = plt.hist(x=data, bins='auto')
+    n, bins, patches = plt.hist(x=data, bins=bins)
     plt.grid(axis='y', alpha=0.75)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
@@ -36,13 +39,13 @@ def histogram(
     plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
     avg = np.round(np.mean(data), 2)
     plt.axvline(avg, color="red", linestyle="--")
-    plt.text(avg, 0, f"mean={avg}", color="red")
+    plt.text(avg + 0.1, 0, f"mean={avg}", color="red")
 
     if alpha:
         high, low = ci.calculate_confidence_interval(data, alpha)
         plt.axvline(high, color="red")
-        plt.text(high, 0, f"{high}", color="red")
+        plt.text(high + 0.1, 0, f"{high}", color="red")
         plt.axvline(low, color="red")
-        plt.text(low, 0, f"{low}", color="red")
+        plt.text(low + 0.1, 0, f"{low}", color="red")
 
     plt.show()
