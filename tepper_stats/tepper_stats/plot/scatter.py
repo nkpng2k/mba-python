dbaff36319e7c32cdff890582349124a35c16a48
Nick Png <nkpng2k@gmail.com>
2023-05-16 22:22:57 -0700

    functioning plot.log_actual_fitted

--- tepper_stats/tepper_stats/plot/scatter.py
+++ tepper_stats/tepper_stats/plot/scatter.py
@@ -14,7 +14,7 @@ def actual_fitted(fitted_model, data: pd.DataFrame, col_name: str, alpha=0.05):
     :param fitted_model: a model created by `.fit()` method from statsmodel package
     :param data: pandas DataFrame with independent and dependent variables
     :param col_name: Name of the dependent variable as a string
-    :param alpha:
+    :param alpha: significance level with which to generate the prediction intervals
     :return:
     """
     target = data[col_name]
@@ -33,8 +33,33 @@ def actual_fitted(fitted_model, data: pd.DataFrame, col_name: str, alpha=0.05):
     plt.show()
 
 
-def log_actual_fitted():
-    pass
+def log_actual_fitted(fitted_model, data: pd.DataFrame, col_name: str, alpha=0.05):
+    """
+    Plots the fitted values of a model for the provided data, where the log of
+    a data column was used as the dependent variable.
+    Ex. dependent variable was log(PRICE) instead of PRICE.
+
+    :param fitted_model: a model created by `.fit()` method from statsmodel package
+    :param data: pandas DataFrame with independent and dependent variables
+    :param col_name: Name of the dependent variable as a string
+    :param alpha: significance level with which to generate the prediction intervals
+    :return:
+    """
+    target = data[col_name]
+    if fitted_model.k_constant != 0:
+        data = sm.add_constant(data[data.columns.drop(col_name)])
+    else:
+        data = data[data.columns.drop(col_name)]
+    predictions = fitted_model.get_prediction(data)
+    pred_summary = predictions.summary_frame(alpha=alpha)
+    pred_summary = pred_summary.apply(lambda x: np.exp(x))
+
+    plt.plot(pred_summary.index, pred_summary["mean"], label="Predictions", color="red")
+    plt.plot(pred_summary.index, target, label="Actual Values", marker="o", color="black")
+    plt.plot(pred_summary.index, pred_summary["obs_ci_lower"], label="Lower Prediction Interval", color="blue")
+    plt.plot(pred_summary.index, pred_summary["obs_ci_upper"], label="Upper Prediction Interval", color="blue")
+    plt.legend()
+    plt.show()
 
 
 def objective_function(fn: Callable, vector: Sequence):
