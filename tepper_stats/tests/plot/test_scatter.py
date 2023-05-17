84296e80cca91432cf69e9b67462f6badcb3f4a6
Nick Png <nkpng2k@gmail.com>
2023-05-16 19:56:56 -0700

    adds test dependencies, ramsey test failing on directory error

---
+++ tepper_stats/tests/plot/test_scatter.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import statsmodels.api as sm
+
+from tepper_stats import plot
+
+
+def test_actual_fitted():
+    pass
+
+
+def test_log_actual_fitted():
+    pass
+
+
+def test_objective_function():
+    pass
