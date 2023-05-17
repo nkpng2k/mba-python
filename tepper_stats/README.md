961f9c8134e50116ec92ea92847afe8a89c954ad
Nick Png <nkpng2k@gmail.com>
2023-05-16 19:08:24 -0700

    completed ramsey_test function, update readme with design decision, small test code incomplete

---
+++ tepper_stats/README.md
@@ -0,0 +1,9 @@
+# Tepper Stats Python Module
+
+This is a port of the R package [here](https://github.com/dnepple/tprstats)
+
+## Design
+
+Current design decisions:
+1. Limited to support of `statsmodels` models - due to the nature of `sklearn`
+package, this module will initially support only `statsmodels` models
--- tepper_stats/tepper_stats/ramsey.py
+++ tepper_stats/tepper_stats/ramsey.py
@@ -1,5 +1,13 @@
 from statsmodels.stats.diagnostic import linear_reset
 
 
-def ramsey_test():
-    pass
+def ramsey_test(fitted_model):
+    one_fitted = linear_reset(fitted_model, power=2, test_type="fitted", use_f=True)
+    two_fitted = linear_reset(fitted_model, power=[2, 3], test_type="fitted", use_f=True)
+
+    print("One Fitted:")
+    print(one_fitted.summary())
+    print("Two Fitted:")
+    print(two_fitted.summary())
+
+    return one_fitted, two_fitted
