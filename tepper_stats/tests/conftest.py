51ee4b67699c3bd5570d07c82b5ccfe936c3c75d
Nick Png <nkpng2k@gmail.com>
2023-05-21 21:22:51 -0700

    adds tests

--- tepper_stats/tests/conftest.py
+++ tepper_stats/tests/conftest.py
@@ -5,7 +5,17 @@ import pandas as pd
 
 
 @pytest.fixture
-def refrigerator_data():
-    test_data_dir = Path(__file__).parent / "data"
-    data_file = test_data_dir / "Refrigerator_Data.xlsx"
+def data_dir():
+    return Path(__file__).parent / "data"
+
+
+@pytest.fixture
+def refrigerator_data(data_dir):
+    data_file = data_dir / "Refrigerator_Data.xlsx"
+    yield pd.read_excel(data_file)
+
+
+@pytest.fixture
+def beer_data(data_dir):
+    data_file = data_dir / "Beer_Data.xlsx"
     yield pd.read_excel(data_file)
